c9c0c4e878e8e46c7f630631dc041815ee38c63c
Duvan Patiño <duvanandres588@gmail.com>
2025-09-25 08:29:03 -0500

    cambios en el código

--- def torres_de_hanoi(n, origen, auxiliar,.py
+++ def torres_de_hanoi(n, origen, auxiliar,.py
@@ -1,14 +1,21 @@
-def torres_de_hanoi(n, origen, auxiliar, destino): #Usamos 3 torres, donde en la primera tendremos todos los 
-    #discos, y la última sera donde se deben organizar, usaremos la segund como ayuda
+def torres_de_hanoi(n, origen, auxiliar, destino, contador):
+   
     if n == 1:
-        print(f"Mover disco 1 de {origen} a {destino}") #Si solo tenemos un disco pues simplemente lo pasamos directamente a la última torre
+        contador[0] += 1
+        print(f"Movimiento {contador[0]}: Mover disco 1 de {origen} a {destino}")
     else:
-        torres_de_hanoi(n-1, origen, destino, auxiliar) #para n discos diferentes a uno empezaremos moviento el disco superior a la torre auxiliar 
-
-        print(f"Mover disco {n} de {origen} a {destino}") #luego movemos el disco más grande a la torre destino
-        torres_de_hanoi(n-1, auxiliar, origen, destino) #y finalmente movemos los discos de la torre auxiliar a la torre destino
+        torres_de_hanoi(n-1, origen, destino, auxiliar, contador)
+        contador[0] += 1
+        print(f"Movimiento {contador[0]}: Mover disco {n} de {origen} a {destino}")
+        torres_de_hanoi(n-1, auxiliar, origen, destino, contador)
 
 # Uso pidiendo al usuario el número de discos:
-
-n_discos = int(input("Ingrese el número de discos: "))
-torres_de_hanoi(n_discos, 'A', 'B', 'C') #A es la torre origen, B la auxiliar y C la destino
+try:
+    n_discos = int(input("Ingrese el número de discos: "))
+    if n_discos < 1 or n_discos > 20:
+        print("El número de discos debe ser mayor o igual a 1 y menor que 20.")
+    else:
+        contador = [0]
+        torres_de_hanoi(n_discos, 'A', 'B', 'C', contador)
+except ValueError:
+    print("Por favor, ingrese un número entero válido.")
